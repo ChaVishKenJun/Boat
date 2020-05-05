2343ba01109b8807e5b6d6b898e86908d647aff0
Junhyeok John Han <junhyeok.john.han@gmail.com>
2020-05-05 16:53:17 +0200

    Menu and page title are cleaned up. Favicon is added.

--- include/frame.php
+++ include/frame.php
@@ -9,7 +9,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
     <link rel="stylesheet" href="css/frame.css">
     
-    <title><?php echo $GLOBALS['title']; ?></title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="image/favicon.ico" type="image/x-icon"/>
+
+    <!-- Title -->`
+    <title>Boat <?php if (!empty($GLOBALS['title'])) { echo ' - ' . $GLOBALS['title']; } ?></title>
   </head>
   <body class="d-flex flex-column h-100">
     <?php include (PATH_INC . "navigation.php"); ?>
@@ -29,7 +33,6 @@
       
       <?php  include $GLOBALS['footer']; ?>
     </div>
-    
     
     <!-- JavaScript -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
--- include/modules/menu.php
+++ include/modules/menu.php
@@ -1,102 +1,46 @@
 <?php
 class Menu {
 	var $menu;
+	
 	function Menu($menu) {
-		if($menu!="" and !is_numeric($menu)) {	
-			$this->BuildNavigation($menu);		
+		if(!empty($menu) and !is_numeric($menu)) {
+			$this->BuildNavigation($menu);
 		} else {
-			$this->BuildNavigation('mHome');	
+			$this->BuildNavigation('mHome');
 		}
 	}
 	
 	public function BuildNavigation($menu) {
 		switch ($menu) {
 			case "mHome":
-				$title="ChaHan Library";
+				$title = "Home";
 				$content = PATH_CONTENT . "home.php";
 				$leftbox = PATH_LEFTBOX . "home.php";
 				$footer = PATH_FOOTER . "footer.php";
-			break 1; 
-			case "mItems":
-				$title="ChaHan Library - Items";
-				$content = PATH_CONTENT . "items.php";
-				$leftbox = PATH_LEFTBOX . "items.php";
+				break;
+			case "mSignIn":
+				$title = "Sign In";
+				$content = PATH_CONTENT . "signIn.php";
+				$leftbox = PATH_LEFTBOX . "home.php";
 				$footer = PATH_FOOTER . "footer.php";
-			break 1; 
-			case "mClients":
-				$title="ChaHan Library - Clients";
-				$content = PATH_CONTENT . "clients.php";
-				$leftbox = PATH_LEFTBOX . "clients.php";
+				break;
+			case "mSignUp":
+				$title = "Sign Up";
+				$content = PATH_CONTENT . "signUp.php";
+				$leftbox = PATH_LEFTBOX . "home.php";
 				$footer = PATH_FOOTER . "footer.php";
-			break 1;
-								
-			case "mSearch":
-				$title="ChaHan Library - Items";
-				$content = PATH_CONTENT . "search.php";
-				$leftbox = PATH_LEFTBOX . "items.php";
-				$footer = PATH_FOOTER . "footer.php";
-			break 1; 
-			case "mcreateOrEditItem":
-				$title="ChaHan Library - Items";
-				$content = PATH_CONTENT . "createOrEditItem.php";
-				$leftbox = PATH_LEFTBOX . "items.php";
-				$footer = PATH_FOOTER . "footer.php";
-			break 1; 
-			case "mcreateOrEditClient":
-					$title="ChaHan Library - Clients";
-					$content = PATH_CONTENT . "createOrEditClient.php";
-					$leftbox = PATH_LEFTBOX . "clients.php";
-					$footer = PATH_FOOTER . "footer.php";
-			break 1; 
-			case "mborrowItem":
-					$title="ChaHan Library - Items";
-					$content = PATH_CONTENT . "borrowItem.php";
-					$leftbox = PATH_LEFTBOX . "items.php";
-					$footer = PATH_FOOTER . "footer.php";
-			break 1;
-			case "mreturnItem":
-					$title="ChaHan Library - Items";
-					$content = PATH_CONTENT . "returnItem.php";
-					$leftbox = PATH_LEFTBOX . "items.php";
-					$footer = PATH_FOOTER . "footer.php";
-			break 1; 
-			case "mreserveItem":
-					$title="ChaHan Library - Items";
-					$content = PATH_CONTENT . "reserveItem.php";
-					$leftbox = PATH_LEFTBOX . "items.php";
-					$footer = PATH_FOOTER . "footer.php";
-			break 1; 
-		case "mSignIn":
-			
-			$title="Boat - Sign in";
-			$content = PATH_CONTENT . "signIn.php";
-			$leftbox = PATH_LEFTBOX . "home.php";
-			$footer = PATH_FOOTER . "footer.php";
-		break 1;
-		case "mSignUp":			
-			$title="Boat - Sign up";
-			$content = PATH_CONTENT . "signUp.php";
-			$leftbox = PATH_LEFTBOX . "home.php";
-			$footer = PATH_FOOTER . "footer.php";
-		break 1;
-		default:
-			$title="ChaHan Library";
-			$content = PATH_CONTENT . "home.php";
-			$leftbox = PATH_LEFTBOX . "home.php";
-			$footer = PATH_FOOTER . "footer.php";
-	}
-		$GLOBALS['title']=$title;
-		$GLOBALS['content']=$content;
-		$GLOBALS['leftbox']=$leftbox;
-		$GLOBALS['footer']=$footer;
+				break;
+			default:
+				echo("Page Not Found");
+		}
+
+		$GLOBALS["title"] = $title;
+		$GLOBALS["content"] = $content;
+		$GLOBALS["leftbox"] = $leftbox;
+		$GLOBALS["footer"] = $footer;
 	}
 }
+
 $menuObj = new Menu($_GET['menu']);
-
-
-
-
-
-
 
 ?>
