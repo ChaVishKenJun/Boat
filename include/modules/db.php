5bae834bc73035086bc8c35c5faf347d280517cf
Junhyeok John Han <junhyeok.john.han@gmail.com>
2020-05-21 18:43:25 +0200

    db has getCurrentDateTime function.

--- include/modules/db.php
+++ include/modules/db.php
@@ -260,6 +260,13 @@ class Database {
 		$this->db_connection->query($sql);
 		$this->db_connection->close();
 	}
+
+	function getCurrentDateTime() {
+		$t = microtime(true);
+		$micro = sprintf("%06d",($t - floor($t)) * 1000000);
+		$d = new DateTime( date('Y-m-d H:i:s.'.$micro, $t) );
+		return $d->format("Y-m-d H:i:s.u");
+	}
 }
 
 $db = new Database;
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 error_reporting(1); // Deactivate all reports
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-require_once("include/constants.php");  
+require_once("include/constants.php");
 ?>
